37c3ed405bd850af6575657876ae43d648ce43ee
Your Name <you@example.com>
2021-03-29 15:34:52 +0530

    session

--- index.js
+++ index.js
@@ -2,12 +2,26 @@
 
 const express = require('express');
 
+const session = require('express-session');
+
 const app = express();
 
 const dataService = require('./services/data.service')
 
-// const app = express();
+app.use(session({
 
+    secret: 'randomsecurestring',
+    resave: false,
+    saveUninitialized: false
+
+}))
+
+// const app = express();
+app.use((req,res,next)=>{
+console.log("middleware")
+next()
+ 
+})
 app.use(express.json());
 
 app.get('/',(req,res)=>{
@@ -23,13 +37,29 @@ app.post('/',(req,res)=>{
 app.post('/register',(req,res)=>{
     console.log(req.body);
     const result = dataService.register(req.body.accno,req.body.username,req.body.password)
-    console.log(res.send(result.message));
+    // console.log(res.send(result.message));
+    // res.status(result.statusCode)
+    console.log(res.status(result.statusCode).json(result));
 })
 app.post('/login',(req,res)=>{
     console.log(req.body);
-    const result = dataService.login(req.body.accno,req.body.pswd)
-    console.log(res.send(result.message));
+    const result = dataService.login(req,req.body.accno,req.body.pswd)
+    // console.log(res.send(result.message));
+    console.log(res.status(result.statusCode).json(result));
 })
+app.post('/deposit',(req,res)=>{
+    console.log(req.session.currentUser);
+    const result = dataService.deposit(req,req.body.accno,req.body.amount,req.body.pswd)
+    // console.log(res.send(result.message));
+    console.log(res.status(result.statusCode).json(result));
+})
+app.post('/withdraw',(req,res)=>{
+    console.log(req.session.currentUser);
+    const result = dataService.withdraw(req,req.body.accno,req.body.amount,req.body.pswd)
+    // console.log(res.send(result.message));
+    console.log(res.status(result.statusCode).json(result));
+})
+
 
 
 app.put('/',(req,res)=>{
--- services/data.service.js
+++ services/data.service.js
@@ -12,12 +12,12 @@ const register = (accno, username, password) => {
 
   if (accno in accountDetails) {
     return {
+      status: true,
+      statusCode: 422,
+ message: "user already exists"
 
-      status: false,
-      message: "user already exists"
-
-
-    }
+}
+    
   }
   else {
     accountDetails[accno] = {
@@ -31,8 +31,8 @@ const register = (accno, username, password) => {
     //   this.saveDetaials();
     console.log(accountDetails);
     return {
-
-      status: true,
+           status: true,
+           statusCode: 200,
       message: "registration successful"
 
     }
@@ -40,16 +40,16 @@ const register = (accno, username, password) => {
   }
 }
 
-const login = (accno, pswd) => {
-  console.log(accno)
-  console.log(pswd)
+const login = (req,accno, pswd) => {
+  // console.log(accno)
+  // console.log(pswd)
   let dataset = accountDetails;
   if (accno in dataset) {
 
     var pswd1 = dataset[accno].password
     console.log(pswd1)
     if (pswd1 == pswd) {
-      currentUser = dataset[accno].username
+     req.session.currentUser = dataset[accno]
 
       return {
 
@@ -57,33 +57,152 @@ const login = (accno, pswd) => {
 
         // this.saveDetaials();
         status: true,
+        statusCode:200,
         message: "login successful"
-
-      }
-    }
-      return {
-
-
-        status: false,
-        message: "incorrect password"
-
 
       }
     }
     return {
 
+      statusCode: 422,
       status: false,
-      message: "no user is exist provide account number "
+      message: "incorrect password"
 
 
     }
+  }
+  return {
+
+    statusCode: 422,
+    status: false,
+    message: "no user is exist provide account number "
 
 
   }
 
+
+}
+
+const deposit = (req,accno, amount, pswd) => {
+  if(!req.session.currentUser){
+    return {
+      status: false,
+      statusCode: 422,
+      message: "Please login"
+    }
+
+  }
+  // console.log(amount)
+  var amt = parseInt(amount);
+  // console.log(amt+5);
+
+  let dataset = accountDetails;
+  if (accno in dataset) {
+
+    var pswd1 = dataset[accno].password
+    // console.log(pswd1)
+    if (pswd1 == pswd) {
+      dataset[accno].balance += amt
+      // this.saveDetaials();
+      // console.log(dataset[accno].balance);
+
+      // alert("Account credited with amount:" + amount+" New balance is :" + dataset[accno].balance);
+
+      return {
+
+        status: true,
+        statusCode: 200,
+        message: "Account has been credited",
+        balance: dataset[accno].balance
+      }
+    }
+    else {
+      return {
+        status: false,
+        statusCode: 422,
+        message: "incorrect password"
+      }
+      // alert("incorrect password")
+
+      // balance:dataset[accno].balance
+
+    }
+  }
+  else {
+    return {
+      // alert("no user exist with provided Account number")
+      status: false,
+      statusCode: 422,
+      message: "no user exist with provided account number"
+    }
+  }
+
+}
+const withdraw = (req,accno, amount, pswd) => {
+  if(!req.session.currentUser){
+    return {
+      status: false,
+      statusCode: 422,
+      message: "please login"
+    }
+  }
+  var amt = parseInt(amount);
+  let dataset = accountDetails;
+  if (accno in dataset) {
+
+    var pswd1 = dataset[accno].password
+    // console.log(pswd1)
+    if (pswd1 == pswd) {
+      if (amount > dataset[accno].amount) {
+        return {
+          status: false,
+          statusCode: 422,
+          message: "insufficient balance"
+
+        }
+      }
+    else {
+        dataset[accno].balance -= amt
+        // this.saveDetaials();
+
+        // alert("Account credited with amount:" + amount+" New balance is :" + dataset[accno].balance);
+
+        return {
+          status: true,
+          statusCode: 200,
+          message: "Account debited",
+          balance:dataset[accno].balance 
+        }
+      }
+    }
+  
+  else {
+      return {
+        status: false,
+        statusCode: 422,
+        message: "incorrect password"
+
+      }
+
+    }
+  }
+else {
+    return {
+      status: false,
+      statusCode: 422,
+      message: "no user exist with provided Account number"
+    }
+
+  }
+}
+
+
+
 module.exports = {
 
   register,
-  login
+  login,
+  deposit,
+  withdraw
 }
 
